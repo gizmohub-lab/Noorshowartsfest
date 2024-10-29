235dbd557b0b6b01a45bd4be4889566496a1f680
gizmohub-lab <gizmo.hub.in@gmail.com>
2024-10-29 15:01:20 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -14,11 +14,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware configuration
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_CONNECT_URI,)
